5d482761cdf0a30d718dd6043f17ec181ed68197
Eric <ericjw@vt.edu>
2017-01-25 21:18:50 -0500

    edited .gitignore, prints hex correctly

--- hexd.cpp
+++ hexd.cpp
@@ -6,15 +6,6 @@
 #include <cstdlib>
 #include <stdint.h>
 
-/*
-using std::string;
-using std::ifstream;
-using std::ofstream;
-using std::cout;
-using std::bitset;
-using std::hex;
-using std::endl;
-*/
 using namespace std;
 
 int main(int argc, char*argv[])
@@ -24,15 +15,31 @@ int main(int argc, char*argv[])
 		std::ifstream instream(argv[1], std::ios::binary);
 
 		uint8_t value;
+		int position = 0;
 		while (!instream.fail())
 		{
-			instream.read(reinterpret_cast<std::fstream::char_type*>(&value), sizeof value);
-			std::bitset<8> set(value);
-			cout << setfill('0') << setw(2) << hex << set.to_ulong() << endl;
-		}
+			
+			printf("%07x:", position); // prints address
+			
 
+			for (int i = 0; i < 16; i++)
+			{
+				instream.read(reinterpret_cast<std::fstream::char_type*>(&value), sizeof value);
+				if (!instream)
+					break;
+				bitset<8> set(value);
+				if (i % 2 == 0)
+					cout << ' ';
+				cout << hex << setfill('0') << setw(2) << set.to_ulong();
+			}
+			cout << endl;
+
+			position += 16;
+		}
+		
 		return EXIT_SUCCESS;
 	}
-	else 
+	else
+		cout << "Incorrect number of arguments" << endl;
 		return EXIT_FAILURE;
 }
