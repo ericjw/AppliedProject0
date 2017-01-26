6ed9820d6076346be3940c2938a4c03d62c9580e
Ubuntu <ubuntu@ubuntu-xenial.localdomain>
2017-01-26 03:23:54 +0000

    seems to work, doesn't pass tests-trying to work with /tests

--- hexd.cpp
+++ hexd.cpp
@@ -13,25 +13,49 @@ int main(int argc, char*argv[])
 	if (argc == 2)
 	{
 		std::ifstream instream(argv[1], std::ios::binary);
-
 		uint8_t value;
 		int position = 0;
+
 		while (!instream.fail())
 		{
-			
-			printf("%07x:", position); // prints address
-			
+			char buff[16];
+			int valid;
 
-			for (int i = 0; i < 16; i++)
+			for (valid = 0; valid < 16 && instream; valid++)
 			{
 				instream.read(reinterpret_cast<std::fstream::char_type*>(&value), sizeof value);
 				if (!instream)
 					break;
 				bitset<8> set(value);
+				buff[valid] = set.to_ulong();
+			}
+			if (valid == 0)
+				break;
+			printf("%07x:", position); // prints address
+			
+			for (int i = 0; i < 16; i++)
+			{
 				if (i % 2 == 0)
 					cout << ' ';
-				cout << hex << setfill('0') << setw(2) << set.to_ulong();
+				if (i < valid)
+				{
+					//cout << hex << setfill('0') << setw(2) << buff[i];
+					printf("%02x", (uint8_t)buff[i]);
+				}
+				else
+					cout << "  ";
 			}
+
+			cout << "  ";
+
+			for (int i = 0; i < valid; i++)
+			{
+				if (buff[i] < 32)
+					cout << '.';
+				else
+					cout << buff[i];
+			}
+			
 			cout << endl;
 
 			position += 16;
